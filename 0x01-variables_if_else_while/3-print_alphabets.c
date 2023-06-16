e6521f8f84504adfa3493d4514cba328d44bff9b
ifydusphil <ifydusphil@gmail.com>
2023-06-16 13:42:37 -0700

    This is 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Displays the alphabet in lowercase and also in uppercase.
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
