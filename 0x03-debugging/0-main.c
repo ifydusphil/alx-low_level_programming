b0f317bff56ec2a13445caeffa3c29901e289f59
ifydusphil <ifydusphil@gmail.com>
2023-06-22 06:11:21 -0700

    This is to commit all the debugging tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
 * main - function to test for positive or negative
 * Return: 0
 */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
+	int i;
+	i = 0;
+	positive_or_negative(i);
+
 return (0);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * largest_number - program that returns the largest of 3 numbers
 * @a: first integer
@@ -9,21 +10,21 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-		if (a > b && a > c)
-		{
+	if (a > b && a > c)
+	{
 		largest = a;
-		}
-		else if (a > b && c > a)
-		{
+	}
+	else if (a > b && c > a)
+	{
 		largest = c;
-		}
-		else if (b > c)
-		{
+	}
+	else if (b > c)
+	{
 		largest = b;
-		}
-		else
-		{
+	}
+	else
+	{
 		largest = c;
-		}
-		return (largest);
+	}
+return (largest);
 }
