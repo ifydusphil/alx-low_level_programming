248459007203477c1b6ce96ec6f96aae6c6a3973
ifydusphil <ifydusphil@gmail.com>
2023-06-22 04:06:32 -0700

    This is for all the mandatory tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints number 0-9 followed by new line
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
