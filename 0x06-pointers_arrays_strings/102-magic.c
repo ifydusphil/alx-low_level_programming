610573abff688d475b87940ddbdcfc477a29a31a
ifydusphil <ifydusphil@gmail.com>
2023-06-29 12:44:01 -0700

    This is 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point of the program and is responsible for
+ * executing the program's logic.
+ *
+ * Return: 0 on success
+ */
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
-
-	a[2] = 1024;
-	p = &n;
+	int a[5] = {0, 1, 2, 3, 4};
+	int *p = a;
 /*
-  * write your line of code here...
-  * Remember:
-  * - you are not allowed to use a
-  * - you are not allowed to modify p
-  * - only one statement
-  * - you are not allowed to code anything else than this line of code
- */
-	*(p + 5) = 98;
- /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+	p += 2;
+	printf("a[2] = %d\n", *p);
 	return (0);
 }
